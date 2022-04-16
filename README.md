b5c5865e7f36a21c656abcb59c2d8429088b593b
Dean Schwartz <deanschwartz2@gmail.com>
2022-04-15 18:57:33 -0700

    Basic server code here

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-### Basic Server Setup
+## Basic Server Setup
 
 ``` npm init -y ```
 
-##### To get node project initialized
+#### To get node project initialized
 
 ``` npm install express ```
 
-##### To install the latest version of express
+#### To install the latest version of express
 
 ``` npm i nodemon --save-dev ```
 
-##### To install nodemon to auto reload the server without starting and stopping
+#### To install nodemon to auto reload the server without starting and stopping
+
+``` npm i morgan ```
+
+#### To install morgan to show status codes in the terminal on each request
+##### 1. Require morgan at the top of app.js just under express
+##### 2. Replace current logging if there with the use of ``` app.use(morgan('dev')); ``` at the top of the use declarations
--- src/app.js
+++ src/app.js
@@ -1,5 +1,58 @@
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+const sayHello = (req, res, next) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : "Hello!";
+    res.send(content);
+};
+
+const saySomething = (req, res) => {
+    const greeting = req.params.greeting;
+    const content =  `${greeting}!`;
+    res.send(content);
+}
+
+app.use(morgan('dev'));
+
+// go to /hello to see output
+app.get('/hello', sayHello);
+
+// go to /say/<AnyGreeting> to see output
+app.get('/say/:greeting', saySomething);
+
+// go to /songs?title=<TitleName> to see output
+app.get('/songs', (req,res) => {
+    //store req title to variable from params
+    const title = req.query.title;
+    //send title
+    res.send(title);
+});
+
+// go to /states/<StateAbbreviation> to see output
+app.get('/states/:abbreviation', (req, res, next) => {
+    //store req abbreviation to variable from params
+    const abbreviation = req.params.abbreviation;
+
+    //validate if the abbreviation is only a length of two
+    if(abbreviation.length !== 2){
+        //call the error handler and pass in error
+        next('State abbreviation is invalid.');
+    } else {
+        //send validated abbreviation
+        res.send(`${abbreviation} is a nice state, I'd like to visit.`);
+    }
+});
+
+// error handler
+app.use((err, req, res, next) => {
+    //log error passed in
+    console.log(err);
+    //send error passed in
+    res.send(err);
+});
 
 module.exports = app;
